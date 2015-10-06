0aa31e294ac8d3f238e317127b7d6f2ad70204d8
Tim Savannah <kata198@gmail.com>
2015-10-06 12:50:36 -0400

    add bogus 0 to version so pypi will accept

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         long_description = f.read()
 
     setup(name='indexedredis',
-        version='2.8.0',
+        version='2.8.0.0',
         packages=['IndexedRedis'],
         install_requires=['redis'],
         requires=['redis'],
