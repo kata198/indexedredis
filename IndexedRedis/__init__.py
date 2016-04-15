7d1620b580db7d1fbe0b7d764aa898632af5d2aa
Tim Savannah <kata198@gmail.com>
2016-04-15 14:48:08 -0400

    Fix the reload method to work, and change return type to show what changed.

--- IndexedRedis/__init__.py
+++ IndexedRedis/__init__.py
@@ -523,31 +523,41 @@ class IndexedRedisModel(object):
 
 	def reload(self):
 		'''
-                reload - Reload this object from the database.
+                reload - Reload this object from the database, overriding any local changes and merging in any updates.
 
                     @raises KeyError - if this object has not been saved (no primary key)
 
-                    @return - True if any updates occured, False if data remained the same.
+                    @return - Dict with the keys that were updated. Key is field name that was updated,
+		       and value is tuple of (old value, new value). 
+
 		'''
 		_id = self._id
 		if not _id:
 			raise KeyError('Object has never been saved! Cannot reload.')
 
 		currentData = self.asDict(False, convertValueTypes=False)
-		newData = self.objects.get(_id)
-		newData.pop('_id', '')
+
+		# Get the object, and compare the unconverted "asDict" repr.
+		#  If any changes, we will apply the already-convered value from
+		#  the object, but we compare the unconverted values (what's in the DB).
+		newDataObj = self.objects.get(_id)
+		if not newDataObj:
+			raise KeyError('Object with id=%d is not in database. Cannot reload.' %(_id,))
+
+		newData = newDataObj.asDict(False, convertValueTypes=False)
 		if currentData == newData:
 			return []
 
-		updatedFieldNames = []
-		# TODO: Check how this works with valueType fields
-		#  also check how this plays with updated fields
-		for fieldName, value in newData.items():
-			if currentData[fieldName] != value:
-				setattr(self, fieldName, value)
-				updatedFieldNames.append(fieldName)
+		updatedFields = {}
+		for fieldName, newValue in newData.items():
+			currentValue = currentData.get(fieldName, '')
+			if currentValue != newValue:
+				# Use "converted" values in the updatedFields dict, and apply on the object.
+				updatedFields[fieldName] = ( getattr(self, fieldName, ''), getattr(newDataObj, fieldName, '') )
+				setattr(self, fieldName, getattr(newDataObj, fieldName, ''))
+				self._origData[fieldName] = newDataObj._origData[fieldName]
 
-		return updatedFieldNames
+		return updatedFields
                     
 
 	def __getstate__(self):
