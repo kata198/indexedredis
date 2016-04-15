9f1a5cc673d9a432d21bbe1c5f40fc4b87f26fce
Tim Savannah <kata198@gmail.com>
2016-04-15 14:59:26 -0400

    Add reload and refetch to IRQueryableList

--- IndexedRedis/IRQueryableList.py
+++ IndexedRedis/IRQueryableList.py
@@ -26,6 +26,7 @@ class IRQueryableList(QueryableListObjs):
 		mdl = self[0].__class__
 		return mdl.deleter.deleteMultiple(self)
 
+
 	def save(self):
 		'''
 			save - Save all objects in this list
@@ -35,7 +36,47 @@ class IRQueryableList(QueryableListObjs):
 		mdl = self[0].__class__
 		return mdl.saver.save(self)
 
-		# TODO: reload and refetch
+
+	def reload(self):
+		'''
+			reload - Reload all objects in this list. 
+				Updates in-place. To just fetch all these objects again, use "refetch"
+
+			@return - List (same order as current objects) of either exception (KeyError) if operation failed,
+			  or a dict of fields changed -> (old, new)
+		'''
+		if len(self) == 0:
+			return []
+		mdl = self[0].__class__
+
+		ret = []
+		for obj in self:
+			res = None
+			try:
+				res = obj.reload()
+			except Exception as e:
+				res = e
+
+			ret.append(res)
+
+		return ret
+
+
+	def refetch(self):
+		'''
+			refetch - Fetch a fresh copy of all items in this list.
+				Returns a new list. To update in-place, use "reload".
+
+			@return IRQueryableList<IndexedRedisModel> - List of fetched items
+		'''
+
+		if len(self) == 0:
+			return IRQueryableList()
+
+		mdl = self[0].__class__
+		pks = [item._id for item in self if item._id]
+
+		return mdl.objects.getMultiple(pks)
 
 
 # vim:set ts=8 shiftwidth=8 softtabstop=8 noexpandtab :
