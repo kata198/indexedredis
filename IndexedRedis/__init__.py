bdbd464971cc118376ce8a5d97f2715c9be1d479
Tim Savannah <kata198@gmail.com>
2016-11-18 00:53:28 -0500

    Update pydoc and tag 4.0.1

--- IndexedRedis/__init__.py
+++ IndexedRedis/__init__.py
@@ -45,10 +45,10 @@ __all__ = ('INDEXED_REDIS_PREFIX', 'INDEXED_REDIS_VERSION', 'INDEXED_REDIS_VERSI
 INDEXED_REDIS_PREFIX = '_ir_|'
 
 # Version as a tuple (major, minor, patchlevel)
-INDEXED_REDIS_VERSION = (4, 0, 0)
+INDEXED_REDIS_VERSION = (4, 0, 1)
 
 # Version as a string
-INDEXED_REDIS_VERSION_STR = '4.0.0'
+INDEXED_REDIS_VERSION_STR = '4.0.1'
 
 # Package version
 __version__ = INDEXED_REDIS_VERSION_STR
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         long_description = summary
 
     setup(name='indexedredis',
-        version='4.0.0',
+        version='4.0.1',
         packages=['IndexedRedis', 'IndexedRedis.fields'],
         install_requires=['redis', 'QueryableList'],
         requires=['redis', 'QueryableList'],
