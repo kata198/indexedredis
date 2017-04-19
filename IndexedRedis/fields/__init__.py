kata198/indexedredis

commit 21d99a399b69224b30b9644e78672a27ee39c89c

Work around a strange bug where these imports would overwrite builtin types...

IndexedRedis/fields/__init__.py
@@ -305,6 +305,13 @@ from .chain import IRFieldChain
 from .b64 import IRBase64Field
 from .fixedpoint import IRFixedPointField
 from .bytes import IRBytesField
+# Prevent these modules from conflicting with builtin / importable types...
+try:
+	del unicode
+	del bytes
+	del pickle
+except:
+	pass
 from .classic import IRClassicField
 
 from .FieldValueTypes import IRDatetimeValue, IRJsonValue

commit a1924b1a7d6f6c728470f240d5175c4c2796f438

Fix regression caused in 21d99a399b69224b30b9644e78672a27ee39c89c on python3

IndexedRedis/fields/__init__.py
@@ -22,11 +22,11 @@ from .null import irNull, IR_NULL_STRINGS, IR_NULL_STR, IR_NULL_BYTES, IR_NULL_U
 
 from hashlib import md5
 
+
 try:
 	unicode
 except NameError:
 	unicode = str
-
 
 
 class IRField(str):
@@ -312,6 +312,12 @@ try:
 	del pickle
 except:
 	pass
+
+try:
+	unicode
+except NameError:
+	unicode = str
+
 from .classic import IRClassicField
 
 from .FieldValueTypes import IRDatetimeValue, IRJsonValue