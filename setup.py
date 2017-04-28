1b1f50eaebce8bf58e4214a5d4fbe635ccdd9d91
Tim Savannah <kata198@gmail.com>
2017-04-28 18:03:52 -0400

    Tag 5.0.0rc4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         long_description = summary
 
     setup(name='indexedredis',
-        version='4.1.4',
+        version='5.0.0rc4',
         packages=['IndexedRedis', 'IndexedRedis.fields'],
         install_requires=['redis', 'QueryableList'],
         requires=['redis', 'QueryableList'],
