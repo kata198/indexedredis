b1ebbe6945bf49e224a7590d0216594aae4e453e
Tim Savannah <kata198@gmail.com>
2017-05-17 09:32:48 -0400

    6.0.2

--- IndexedRedis/__init__.py
+++ IndexedRedis/__init__.py
@@ -41,10 +41,10 @@ __all__ = ('INDEXED_REDIS_PREFIX', 'INDEXED_REDIS_VERSION', 'INDEXED_REDIS_VERSI
 INDEXED_REDIS_PREFIX = '_ir_|'
 
 # Version as a tuple (major, minor, patchlevel)
-INDEXED_REDIS_VERSION = (6, 0, 1)
+INDEXED_REDIS_VERSION = (6, 0, 2)
 
 # Version as a string
-INDEXED_REDIS_VERSION_STR = '6.0.1'
+INDEXED_REDIS_VERSION_STR = '6.0.2'
 
 # Package version
 __version__ = INDEXED_REDIS_VERSION_STR
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         long_description = summary
 
     setup(name='indexedredis',
-        version='6.0.1',
+        version='6.0.2',
         packages=['IndexedRedis', 'IndexedRedis.fields'],
         install_requires=['redis', 'QueryableList'],
         requires=['redis', 'QueryableList'],
